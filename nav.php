d4d55c254a8e65b254773ff77ff26304be9f5a8b
DW453 <45468083+DW453@users.noreply.github.com>
2020-07-22 13:27:18 -0500

    added nav/footer.php and removed master.html

---
+++ nav.php
@@ -0,0 +1,17 @@
+<nav>
+  <div class="logo">
+    <h1 class="nav">Wapello Trails</h1>
+  </div>
+
+    <ul class="nav-links">
+      <li><a href="home.html">Home</a></li>
+      <li><a href="gallery.html">Gallery</a></li>
+      <li><a href="trails.html">Trails</a></li>
+    </ul>
+
+      <div class="burger">
+        <div class="line1"></div>
+        <div class="line2"></div>
+        <div class="line3"></div>
+      </div>
+</nav>
