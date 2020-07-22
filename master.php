3dc6d43a7e5cf6c437864fa364c542ea2fcc9e4b
DW453 <45468083+DW453@users.noreply.github.com>
2020-07-22 14:23:38 -0500

    push test

---
+++ master.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Wapello Trails</title>
+<link rel="stylesheet" href="css/masterstyle.css?v=<?=time();?>">
+</head>
+<body>
+	<?php
+		include 'nav.php';
+	?>
+
+	<div id="content">
+		<h1>Content</h1>
+		<h1>Content</h1>
+		<h1>Content</h1>
+		<h1>Content</h1>
+		<h1>Content</h1>
+		<h1>Content</h1>
+		<h1>Content</h1>
+		<h1>Content</h1>
+		<h1>Content</h1>
+		<h1>Content</h1>
+		<h1>Content</h1>
+		<h1>Content</h1>
+		<h1>Content</h1>
+		<h1>Content</h1>
+		<h1>Content</h1>
+		<h1>Content</h1>
+		<h1>Content</h1>
+		<h1>Content</h1>
+		<h1>Content</h1>
+		<h1>Content</h1>
+		<h1>Content</h1>
+		<h1>Content</h1>
+		<h1>Content</h1>
+		<h1>Content</h1>
+		<h1>Content</h1>
+		<h1>Content</h1>
+		<h1>Content</h1>
+		<h1>Content</h1>
+		<h1>Content</h1>
+		<h1>Content</h1>
+		<h1>Content</h1>
+		<h1>Content</h1>
+		<h1>Content</h1>
+		<h1>Content</h1>
+	</div>
+
+	<?php
+		include 'footer.php';
+	 ?>
+</body>
+</html>
