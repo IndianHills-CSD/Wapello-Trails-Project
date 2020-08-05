a870903d3183be5c59cd4494a27f5b17944b8d87
themorticianer <lucrurup@gmail.com>
2020-08-05 13:58:40 -0500

    strat. plans added

---
+++ strategicplans.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/Style.php">
+	<link rel="stylesheet" href="css/masterstyle.css">
+	<title>Home</title>
+</head>
+<body>
+
+	<?php include 'nav.php';?>
+	
+	
+	<?php include 'footer.php';?>
+</body>
+ </html>
