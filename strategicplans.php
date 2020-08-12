43543cb95cbd6394e86589120bef8c79c62952f9
themorticianer <lucrurup@gmail.com>
2020-08-12 12:50:35 -0500

    basic strat plans

--- strategicplans.php
+++ strategicplans.php
@@ -9,8 +9,40 @@
 <body>
 
 	<?php include 'nav.php';?>
-	
-	
+	<div class ="conContainer">
+	<img class ="center" src="images/future-plan-page1-1000x1500.jpg"/>
+	<p class ="Plandesc">
+	Laborum itaque necessitatibus minima odio. Est deserunt dicta rerum sed magni delectus. Enim voluptatem dicta id dolor consequuntur laborum eaque beatae. Eveniet dolorem corporis aut impedit sed praesentium. Non impedit reiciendis nisi qui labore.
+
+	Sapiente quo qui repellat ea numquam sit et. Dicta rerum ut reprehenderit eveniet in corrupti reiciendis. Ducimus delectus enim dolor qui corrupti sit ex itaque.
+
+	Et consequatur eos sunt sit sunt asperiores aut. Voluptatem eos ut velit corporis omnis. Nihil quis laudantium quos. Vitae debitis autem esse. Suscipit ipsam et eaque distinctio ut impedit maiores natus.
+	</p>
+	<img class="center" src ="images/future-plan-page2-1000x1500.jpg">
+	<p class ="Plandesc">
+	Laborum itaque necessitatibus minima odio. Est deserunt dicta rerum sed magni delectus. Enim voluptatem dicta id dolor consequuntur laborum eaque beatae. Eveniet dolorem corporis aut impedit sed praesentium. Non impedit reiciendis nisi qui labore.
+
+	Sapiente quo qui repellat ea numquam sit et. Dicta rerum ut reprehenderit eveniet in corrupti reiciendis. Ducimus delectus enim dolor qui corrupti sit ex itaque.
+
+	Et consequatur eos sunt sit sunt asperiores aut. Voluptatem eos ut velit corporis omnis. Nihil quis laudantium quos. Vitae debitis autem esse. Suscipit ipsam et eaque distinctio ut impedit maiores natus.
+	</p>
+	<img class="center" src ="images/future-plan-page3-1000x1500.jpg">
+	<p class ="Plandesc">
+	Laborum itaque necessitatibus minima odio. Est deserunt dicta rerum sed magni delectus. Enim voluptatem dicta id dolor consequuntur laborum eaque beatae. Eveniet dolorem corporis aut impedit sed praesentium. Non impedit reiciendis nisi qui labore.
+
+	Sapiente quo qui repellat ea numquam sit et. Dicta rerum ut reprehenderit eveniet in corrupti reiciendis. Ducimus delectus enim dolor qui corrupti sit ex itaque.
+
+	Et consequatur eos sunt sit sunt asperiores aut. Voluptatem eos ut velit corporis omnis. Nihil quis laudantium quos. Vitae debitis autem esse. Suscipit ipsam et eaque distinctio ut impedit maiores natus.
+	</p >
+	<img class="center" src ="images/future-plan-page4-1000x1500.jpg">
+	<p class ="Plandesc">
+	Laborum itaque necessitatibus minima odio. Est deserunt dicta rerum sed magni delectus. Enim voluptatem dicta id dolor consequuntur laborum eaque beatae. Eveniet dolorem corporis aut impedit sed praesentium. Non impedit reiciendis nisi qui labore.
+
+	Sapiente quo qui repellat ea numquam sit et. Dicta rerum ut reprehenderit eveniet in corrupti reiciendis. Ducimus delectus enim dolor qui corrupti sit ex itaque.
+
+	Et consequatur eos sunt sit sunt asperiores aut. Voluptatem eos ut velit corporis omnis. Nihil quis laudantium quos. Vitae debitis autem esse. Suscipit ipsam et eaque distinctio ut impedit maiores natus.
+	</p >
+	</div>
 	<?php include 'footer.php';?>
 </body>
  </html>
