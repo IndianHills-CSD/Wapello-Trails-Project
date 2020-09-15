6cb6eacfabfa799775ec932ded9fb60f217a83d5
themorticianer <lucrurup@gmail.com>
2020-09-15 10:10:07 -0500

    table updated

---
+++ Milestones.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/Style.php">
+	<link rel="stylesheet" href="css/masterstyle.css">
+	<title>Home</title>
+</head>
+<body>
+<table class="milestones">
+				  <tr>
+					<th>Wapello County Trails Segments</th>
+					<th>Year Constructed</th>
+					<th>Primary Funding</th>
+					<th>Trails Segment Cost</th>
+				  </tr>
+				  <tr>
+					<td>Ottumwa Park Trail</td>
+					<td>1998</td>
+					<td>Federal Enhancement Funds</td>
+					<td>$99,238.42</td>
+				  </tr>
+				  <tr>
+					<td>South Levee Trails (Market to Quincy)</td>
+					<td>2002</td>
+					<td>Federal Enhancement Funds</td>
+					<td>$101,856.66 </td>
+				  </tr>
+				  <tr>
+					<td>Wabash Bridge Acquisition</td>
+					<td>2006</td>
+					<td>Wapello County</td>
+					<td>  </td>
+				  </tr>
+				  <tr>
+					<td>Wabash Bridge Decking</td>
+					<td>2007</td>
+					<td>Wapello County Trails Council</td>
+					<td>$30,000.00 </td>
+				  </tr>
+				  <tr>
+					<td>North Levee Trails (Market to Vine)</td>
+					<td>2007</td>
+					<td>Federal Enhancement Funds</td>
+					<td>$52,746.00 </td>
+				  </tr>
+				  <tr>
+					<td>North Levee Trails (Market to Vine)</td>
+					<td>2007</td>
+					<td>Wapello County Foundation</td>
+					<td>$15,000.00 </td>
+				  </tr>
+				  <tr>
+					<td>South Loop around John Deere Segment 1</td>
+					<td>2007</td>
+					<td>State Enhancement Funds</td>
+					<td>$221,429.80 </td>
+				  </tr>
+				  <tr>
+					<td>Concrete Extension (300 Lineal Feet)</td>
+					<td>2007</td>
+					<td>Wapello County Foundation</td>
+					<td>$15,000.00 </td>
+				  </tr>
+				  <tr>
+					<td>Gray Eagle Extension (Vine to Preserve)</td>
+					<td>2008</td>
+					<td>Federal Enhancement Funds</td>
+					<td>$209,262.00 </td>
+				  </tr>
+				  <tr>
+					<td>Bike Trails - Gray Eagle (Vine St to Gray Eagle)</td>
+					<td>2008</td>
+					<td>Wapello County Trails</td>
+					<td>$2,929.00 </td>
+				  </tr>
+				  <tr>
+					<td>JD Loop Segment 4(Wabash Bridge to John Deere)</td>
+					<td>2009</td>
+					<td>REAP Funds</td>
+					<td>$149,999.91 </td>
+				  </tr>
+				  <tr>
+					<td>Turkey Island Extension</td>
+					<td>2009</td>
+					<td>REAP Funds</td>
+					<td>$114,226.00 </td>
+				  </tr>
+				  <tr>
+					<td>Gray Eagle Extension (Iowa Ave to City Limits)</td>
+					<td>2010</td>
+					<td>Federal Enhancement Funds</td>
+					<td>$180,031.00 </td>
+				  </tr>
+				  <tr>
+					<td>JD Loop Segment 2</td>
+					<td>2010</td>
+					<td>Federal Enhancement Funds</td>
+					<td>$245,034.93 </td>
+				  </tr>
+				  <tr>
+					<td>LED Solar Lighting - John Deere Loop</td>
+					<td>2011</td>
+					<td>John Deere Foundation</td>
+					<td>  </td>
+				  </tr>
+				  <tr>
+					<td>Market Street Bridge - Addition of Trail Crossing</td>
+					<td>2014</td>
+					<td>Wapello County Trails CouncilWapello County Trails Council</td>
+					<td>$40,000.00 </td>
+				  </tr>
+				  <tr>
+					<td>Hwy 34 Underpass - Between Ottumwa Park & Wayside Park</td>
+					<td>2015</td>
+					<td>Legacy Foundation, City of 
+						Ottumwa, Wapello County, Vision 
+						Iowa - C.A.T. Project,  John Deere 
+						Foundation, Wapello County Foundation,  Community Donations.
+					</td>
+					<td>$654,375.00 </td>
+				  </tr>
+				  <tr>
+					<td>  </td>
+					<td>  </td>
+					<td> Total Invested in Wapello County Trails to Date:</td>
+					<td>$2,131,128.72</td>
+				  </tr>
+				  
+			</table>
+</body>
+</html>
