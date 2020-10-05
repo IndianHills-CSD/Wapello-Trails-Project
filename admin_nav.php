55a85759a2c6d66d54311760469731a16c6e7434
DW453 <45468083+DW453@users.noreply.github.com>
2020-10-05 11:48:28 -0500

    admin nav update

--- admin_nav.php
+++ admin_nav.php
@@ -4,9 +4,10 @@
     <h1 class="nav">Wapello County Trails Council(admin)</h1>
   </div>
   <ul class="nav-links">
-    <li><a href="index.php">Home</a></li>
     <li><a href="#">Email</a></li>
-    <li><a href="admin_login.php">Admin Login</a></li>
+    <li><a href="#">Marquee Update</a></li>
+    <li><a href="#">Credential Change</a></li>
+    <li><a href="admin_logout.php">Admin Logout</a></li>
   </ul>
   <div class="burger">
     <div class="line1"></div>
