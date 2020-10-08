d75d7edf57f4892fed6befd284915f5c50fd57dc
[Josh] <48997663+jjdelong@users.noreply.github.com>
2020-10-08 11:10:18 -0500

    PW change form

--- admin_login.php
+++ admin_login.php
@@ -18,7 +18,9 @@
         </head>
 
         <body>
+
         <?php include 'nav.php';?>
+
             <div class="form-container">
             
             <?php
@@ -40,38 +42,28 @@
                 <img class="login-icon" src="images/user-logo.png">
                 <h1>Admin Log In</h1>
                 <form name="sign-in" id="sign-in" role="form" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
-                <label for="username">Username:</label>
-                <input type="text" name="username" class="input-box" pattern="[a-zA-Z0-9]+" required>
-                <label for="password">Password:</label>
-                <input type="password" name="password" class="input-box" id="input-box" required>
-                <input type="checkbox" name="toggle" onclick="toggle_vis()" class=" password-toggle">
-                <label for="toggle">Show Password</label>
-                <p id="val-label" class="val-label" style="color:red"><strong><?php echo $msg; ?></strong></p>
-                <input type="submit" name="submit" value="Submit" class="sign-in-btn">
-                <input type="button" onclick="reset()" value="Reset" class="reset-btn">
 
-                    <script>
-                        // Javascript to toggle password visibilty 
-                        function toggle_vis() {
-                            var x = document.getElementById("input-box");
-                            if (x.type === "password") {
-                                x.type = "text";
-                            } else {
-                                x.type = "password";
-                            }
-                        }
+                    <!-- Textboxes and Labels -->
+                    <label for="username">Username:</label>
+                    <input type="text" name="username" class="inputBox" pattern="[a-zA-Z0-9]+" required>
+                    <label for="password">Password:</label>
+                    <input type="password" name="password" class="inputBox required" id="pw_Vis" required>
 
-                       // Reset form  
-                       function reset() {
-                           document.getElementById("sign-in").reset();
-                       } 
-                    </script>
+                    <!-- Checkbox and Buttons -->
+                    <input type="checkbox" name="toggle" onclick="toggle_vis()" class=" password-toggle">
+                    <label for="toggle">Show Password</label>
+                    <p id="val-label" class="val-label" style="color:red"><strong><?php echo $msg; ?></strong></p>
+                    <input type="submit" name="submit" value="Submit" class="sign-in-btn">
+                    <input type="button" onclick="reset()" value="Reset" class="reset-btn">
                 </form>
+
                 <!--
                        Link button to clear username and pw from session data when clicked. This will be handled by logout button following its implementation.
                 -->
                 Click <a href = "admin_logout.php" tite = "Logout" style="color: blue">HERE </a>to clean session data. This will be handled by a Logout button later.
             </div>
+
             <?php include 'footer.php';?>
+            <script type="text/javascript" src="js/logScripts.js"></script>
         </body>
     </html>
